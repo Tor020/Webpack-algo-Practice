641b1b19d8463a1408886ef6b0f8fc4dc6f26a03
= <=>
2018-02-12 19:22:25 -0500

    's'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ if (module.hot) {module.hot.accept({options:false})}
 
 import {name, testExport} from './Core/example'; // Imports  https://stackoverflow.com/questions/38077164/es6-export-from-import
 
+name();
+testExport();
 
 
-console.log('ye');
+
